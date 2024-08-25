242351b3dee2fbba97835e4373c99a103ba5c23c
milanthapa12 <milanthapait@gmail.com>
2024-08-25 14:53:45 +0545

    datatype issue

--- src/app/components/post/mutations.ts
+++ src/app/components/post/mutations.ts
@@ -2,7 +2,7 @@ import { useToast } from "@/components/ui/use-toast"
 import { InfiniteData, QueryFilters, useMutation, useQueryClient } from "@tanstack/react-query"
 import { usePathname, useRouter } from "next/navigation"
 import { deletePost } from "./actions"
-import { PostData } from "@/lib/type"
+import { PostPage } from "@/lib/type"
 
 export function useDeletePostMutation() {
 
@@ -16,20 +16,19 @@ export function useDeletePostMutation() {
         onSuccess: async (deletedPost) => {
             const queryFilter: QueryFilters = { queryKey: ["post-feed"] }
             await queryClient.cancelQueries(queryFilter)
-            queryClient.setQueriesData<InfiniteData<PostData, string | null>>(
+            queryClient.setQueriesData<InfiniteData<PostPage, string | null>>(
                 queryFilter,
                 (oldData) => {
-                    if (!oldData) return
+                    if (!oldData) return;
                     return {
                         pageParams: oldData.pageParams,
-                        pages: oldData.pages.map(page => ({
+                        pages: oldData.pages.map((page) => ({
                             nextCursor: page.nextCursor,
-                            posts: page.posts.filter(p => p.id !== deletedPost.id)
-
-                        }))
-                    }
-                }
-            )
+                            posts: page.posts.filter((p) => p.id !== deletedPost.id),
+                        })),
+                    };
+                },
+            );
             toast({
                 description: "Post deleted successfully"
             })
